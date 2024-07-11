ec01a8091b63ea0a335cd073075da25d10c0aed5
maksim_repnikov <maks.repnikov32@mail.ru>
2024-07-12 02:32:55 +0300

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { ItemComponent } from './item/item.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: MainComponent
+  },
+  {
+    path:'contact',
+    component: ContactComponent
+  },
+  {
+    path:'about',
+    component: AboutComponent
+  },
+  {
+    path:'item',
+    component: ItemComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { ItemComponent } from './item/item.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [				
+    AppComponent,
+      ContactComponent,
+      AboutComponent,
+      ItemComponent,
+      MainComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
