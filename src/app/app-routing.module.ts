e7e2588c6f2a905efce0e8950dcdd9a595f0165f
maksim_repnikov <maks.repnikov32@mail.ru>
2024-07-13 18:36:27 +0300

    Add routing main

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
 
 
 const routes: Routes = [
   {
     path: 'item',
     loadChildren: () => import('./page/page.module').then(m => m.PageModule)
+  },
+  {
+    path: 'main',
+    component: MainComponent
   }
 ];
 
