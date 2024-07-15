dfbca6f3d40006c1eb58eda3793ee337e202506d
maksim_repnikov <maks.repnikov32@mail.ru>
2024-07-15 17:40:27 +0300

    Add Service and testBook object

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookComponent } from './book/book.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      BookComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
