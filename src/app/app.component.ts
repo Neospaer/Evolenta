76c305b05d3bba63149a42e99f120ac24a9dd3b8
maksim_repnikov <maks.repnikov32@mail.ru>
2024-07-20 21:02:18 +0300

    Add Variable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ResponseService } from './response.service';
+import { Users } from './interface/Users';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  ArrUser: Users[] = []
   
   constructor(public responseServ: ResponseService){
+  }
+
+  ngOnInit(): void {
+    this.ArrUser = this.responseServ.arr
   }
 }
