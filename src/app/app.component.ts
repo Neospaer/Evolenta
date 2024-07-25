c512058256333e74ca9b9d02de7900c2ddc999c7
maksim_repnikov <maks.repnikov32@mail.ru>
2024-07-25 19:59:40 +0300

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval, map, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FirstEvoApp';
+  
+  sequenceNumbers: number[] = [];
+  randomNumbers: string[] = [];
+  sequenceSubscription: Subscription | null = null;
+  randomSubscription: Subscription | null = null;
+
+  startSequence() {
+    if (!this.sequenceSubscription) {
+      this.sequenceSubscription = interval(2000).subscribe(num => {
+        this.sequenceNumbers.push(num);
+      });
+    }
+  }
+
+  startRandom() {
+    if (!this.randomSubscription) {
+      this.randomSubscription = interval(2000).pipe(
+        map(() => `Random Value: ${Math.floor(Math.random() * 10000)}`)
+      ).subscribe(value => {
+        this.randomNumbers.push(value);
+      });
+    }
+  }
+
+  stopSequence() {
+    this.sequenceSubscription?.unsubscribe();
+    this.sequenceSubscription = null;
+  }
+
+  stopRandom() {
+    this.randomSubscription?.unsubscribe();
+    this.randomSubscription = null;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ButtonsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
