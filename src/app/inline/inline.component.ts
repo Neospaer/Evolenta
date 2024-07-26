0a617df5b178ad0d7e2935766df5e55a8c832ad5
maksim_repnikov <maks.repnikov32@mail.ru>
2024-07-26 20:19:22 +0300

    Add meta ted on inline

--- src/app/inline/inline.component.ts
+++ src/app/inline/inline.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Meta, Title } from '@angular/platform-browser';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-inline',
@@ -8,14 +8,11 @@ import { Meta, Title } from '@angular/platform-browser';
 })
 export class InlineComponent implements OnInit {
 
-  constructor(private meta: Meta,private title: Title) { 
+  constructor(private title: Title) { 
   }
 
   ngOnInit(): void {
     this.title.setTitle('Open Graph Page')
-    this.meta.addTag({property: 'og:title',content: 'The Rock'})
-    this.meta.addTag({property: 'og:type', content: 'video.movie'})
-    this.meta.addTag({property: 'og:url', content: '//www.imdb.com/title/tt0117500/'})
   }
 
 }
