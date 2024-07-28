b2d86d52244e4504603f2dba9bc9cab36456922a
maksim_repnikov <maks.repnikov32@mail.ru>
2024-07-29 00:29:00 +0300

    Fixed Error

--- src/app/add/add.component.ts
+++ src/app/add/add.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { DataService } from '../data.service';
+import { AddTodo } from '../store/todo.action';
+import { Store } from '@ngxs/store';
+
 
 @Component({
   selector: 'app-add',
@@ -13,7 +16,8 @@ export class AddComponent implements OnInit {
 
   constructor(
     private todoService: DataService,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private store: Store
   ) {}
 
   ngOnInit() {
@@ -23,10 +27,12 @@ export class AddComponent implements OnInit {
   }
 
   addTasks() {
-    this.todoService.create({
-      value: this.todoForm.controls['todo'].value,
-      completed: false,
-    });
-    this.todoForm.reset();
+    if (this.todoForm.valid) {
+      this.store.dispatch(new AddTodo({
+        value: this.todoForm.controls['todo'].value,
+        completed: false,
+      }));
+      this.todoForm.reset();
+    }
   }
 }
