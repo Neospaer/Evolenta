4828ebfb837472cc020ebbc746e6f45b899917e0
maksim_repnikov <maks.repnikov32@mail.ru>
2024-07-30 00:04:42 +0300

    Add bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FirstEvoApp';
+  onValueChange(value: any): void {
+    console.log('onValueChange', value);
+  }
 }
